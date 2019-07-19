720257a11457abc957b53278ddef738abb456b43
Jason Shockness <jshock12@gmail.com>
2019-07-19 13:33:21 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,7 +5,15 @@ class Song
   
   def initialize(name)
     @name = name
-    @@all = 
+    @@all << self
   end
   
+  def self.all
+    @@all
+  end
+
+  def artist_name
+    artist.name if artist
+  end
+end
   
