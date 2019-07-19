42547a72cdc9b1721e4a8715304d2a38666aaa92
Jason Shockness <jshock12@gmail.com>
2019-07-19 13:32:06 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,17 @@ class Artist
     song.artist = self
   end
   
-  def 
-    
-    
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
     
 end
