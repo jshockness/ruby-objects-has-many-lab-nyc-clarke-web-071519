df97addc6fc5aa21673e22379c5526321c534f7d
Jason Shockness <jshock12@gmail.com>
2019-07-19 13:22:58 +0000

    Automatically backed up by Learn

---
+++ lib/artist.rb
@@ -0,0 +1,17 @@
+class Artist 
+  attr_accessor :name 
+  
+  def initialize(name)
+    @name = name 
+  end
+  
+  def song 
+    @song = song
+  end
+  
+  def add_song
+  end
+    
+    
+    
+end
